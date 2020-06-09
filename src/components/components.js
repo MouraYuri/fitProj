f995e989678cddf4099a9cdcb3292553c8f96e68
Moura Yuri <mourayuri339@gmail.com>
2020-06-09 14:58:18 -0300

    main container created

--- src/components/components.js
+++ src/components/components.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import styled from 'styled-components/native';
 
+import LinearGradient from 'react-native-linear-gradient';
+
 export const Container = styled.View`
     flex: 1
     flex-direction: column
     background-color: #0f0e0e
     align-items: center
 `
+
+export const Container2 = (props) => {
+    return(
+        <LinearGradient colors={['#02010a', '#17161a']}
+        style={{
+            flex:1,
+            flexDirection: 'column',
+            alignItems:'center'
+        }}>
+            {props.children}
+        </LinearGradient>
+    )
+}
